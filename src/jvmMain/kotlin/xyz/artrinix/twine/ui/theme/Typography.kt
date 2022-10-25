6cd53bc650628ea1183350b8c1668a00e03db9e2
Asoji <asoji-git@hiitsdevin.mozmail.com>
2022-10-25 00:48:18 -0700

    🍱 Added Manrope font

--- src/jvmMain/kotlin/xyz/artrinix/twine/ui/theme/Typography.kt
+++ src/jvmMain/kotlin/xyz/artrinix/twine/ui/theme/Typography.kt
@@ -2,78 +2,102 @@ package xyz.artrinix.twine.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.unit.sp
+
+val Manrope = FontFamily(
+    Font(resource = "fonts/Manrope-ExtraLight.ttf"),
+    Font(resource = "fonts/Manrope-Light.ttf"),
+    Font(resource = "fonts/Manrope-Regular.ttf"),
+    Font(resource = "fonts/Manrope-Medium.ttf"),
+    Font(resource = "fonts/Manrope-SemiBold.ttf"),
+    Font(resource = "fonts/Manrope-Bold.ttf"),
+    Font(resource = "fonts/Manrope-ExtraBold.ttf")
+    )
 
 val appTypography = Typography(
     headlineLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
+        fontFamily = Manrope,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp
     ),
     headlineMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
+        fontFamily = Manrope,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp
     ),
     headlineSmall = TextStyle(
         fontWeight = FontWeight.SemiBold,
+        fontFamily = Manrope,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp
     ),
     titleLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
+        fontFamily = Manrope,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp
     ),
     titleMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
+        fontFamily = Manrope,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     titleSmall = TextStyle(
         fontWeight = FontWeight.Bold,
+        fontFamily = Manrope,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     bodyLarge = TextStyle(
         fontWeight = FontWeight.Normal,
+        fontFamily = Manrope,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.15.sp
     ),
     bodyMedium = TextStyle(
         fontWeight = FontWeight.Medium,
+        fontFamily = Manrope,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp
     ),
     bodySmall = TextStyle(
         fontWeight = FontWeight.Bold,
+        fontFamily = Manrope,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp
     ),
     labelLarge = TextStyle(
         fontWeight = FontWeight.SemiBold,
+        fontFamily = Manrope,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp
     ),
     labelMedium = TextStyle(
         fontWeight = FontWeight.SemiBold,
+        fontFamily = Manrope,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
     ),
     labelSmall = TextStyle(
         fontWeight = FontWeight.SemiBold,
+        fontFamily = Manrope,
         fontSize = 11.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp
